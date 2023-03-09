fa0d5d1f3ccb09c7bdf4cf3b79908ad457174d96
Karol Wereski <karol.wereski@o2.pl>
2023-03-09 22:06:18 +0100

    Add javaFX_template

---
+++ src/main/java/com/projekt/projekt_l3_g3_/entities/Users.java
@@ -0,0 +1,5 @@
+package com.projekt.projekt_l3_g3_.entities;
+
+public class Users {
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.projekt.projekt_l3_g3_ {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.persistence;
+    requires org.hibernate.orm.core;
 
-
+    opens com.projekt.projekt_l3_g3_.entities to org.hibernate.orm.core;
+    exports com.projekt.projekt_l3_g3_.entities;
     opens com.projekt.projekt_l3_g3_ to javafx.fxml;
     exports com.projekt.projekt_l3_g3_;
 }
